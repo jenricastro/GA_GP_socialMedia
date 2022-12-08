ff323f5188bd6f5b6984bece71b5df5a3c68f931
Jaime Castro <jenri@MacBook-Pro.lan>
2022-12-08 11:32:18 -0500

    user connected to db

--- server/models/User.js
+++ server/models/User.js
@@ -47,14 +47,7 @@ const userSchema = new mongoose.Schema({
             'Password must be 8 characters or longer'
         ]
     },
-
-    //We can add in a custom validator called validate into our schema in the same sort of way that we have required or minlength and we need to provide it an object that contains a validator function (something that will return true or false) and the message we want to display if the value is not valid.
-
-    validate: {
-        validator: val => /^([\w-\.]+@([\w-]+\.)+[\w-]+)?$/.test(val),
-        message: "Please enter a valid email"
-      }
-      
+   
 }, {timestamps: true})
 
 
