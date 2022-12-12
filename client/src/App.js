035ef04fd0c74dba26223794fade35edc75ce094
Jaime Castro <jenri@MacBook-Pro.lan>
2022-12-11 19:43:20 -0500

    client able to post

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,43 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './components/user/login/login';
-import Register from './components/user/register/register';
-import ShowPost from './components/media/ShowPost';
+// import Login from './components/user/login/login';
+// import Register from './components/user/register/register';
+// import ShowPost from './components/media/ShowPost';
+import Main from './view/Main';
 
 const App = () =>{
+  
 
-
-  const [ user, setLoginUser] = useState({})
-
-  useEffect(() => {
-    setLoginUser(JSON.parse(localStorage.getItem("User")))
-  }, [])
-
-
-
-
+//   const [ user, setLoginUser] = useState({})
+// 
+//   useEffect(() => {
+//     setLoginUser(JSON.parse(localStorage.getItem("User")))
+//   }, [])
 
 
   return(
+
+ <>
+
+  {/* {post.map((post)=>{
+  return( */}
     <>
-    {/* add frontend (add + edit + show)
-
-
-
-
-
-
-
-
-    //--------------------- */}
+    
     <BrowserRouter>
       <Routes>
-          <Route path='/' element={<ShowPost/>}/>
-          <Route path="/login" element={<Login setLoginUser={setLoginUser}/>} />
-          <Route path="/register" element ={<Register />}/>
+          <Route path = '/' element={<Main/>}></Route>
+          {/* <Route path='/' element={<ShowPost/>}/> */}
+          {/* <Route path="/login" element={<Login setLoginUser={setLoginUser}/>} />
+          <Route path="/register" element ={<Register />}/> */}
         
       </Routes>
     </BrowserRouter>
     </>
-  )
+    
+  {/* })} */}
+    </>
+ )
 }
 
 export default App
